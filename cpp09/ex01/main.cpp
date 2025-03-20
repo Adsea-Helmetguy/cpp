8567d05091d86a7aa1260538283cee89f32cb719
mlow <marcuslowsj@gmail.com>
2025-03-20 18:31:18 +0800

    start coding

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -12,7 +12,34 @@
 
 #include "RPN.hpp"
 
+static	bool	numtoken_checker(int argc, char ** argv)
+{
+	num
+	return (true);
+};
+
+//3 − 4 + 5
+//3 4 − 5 +
 int	main(int argc, char **argv)
 {
+	//check if the values are numbers or Multiplication Table(+ - / *)
+	if (argc < 10)
+	{
+		if (!(numtoken_checker(argc, argv)))
+		{
+			std::cerr << RED << "THIS AIN'T a number!!" << RT << std::endl;
+			return (1);
+		}
+
+		//start coding
+		RPN_code(argv);
+	}
+	else
+	{
+		std::cerr << YELLOW << "Less than 10 arguments needed!" \
+			<< RT << std::endl;
+	}
 	return (0);
 };
+
+
