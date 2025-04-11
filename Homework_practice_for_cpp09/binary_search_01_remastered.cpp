020d5847e2585bfa2642e43de2861f3661d951ce
Mlow <marcuslowsj@gmail.com>
2025-04-11 18:58:46 +0800

    updated code

--- Homework_practice_for_cpp09/binary_search_01_remastered.cpp
+++ Homework_practice_for_cpp09/binary_search_01_remastered.cpp
@@ -19,17 +19,17 @@ void	print_vector(std::vector<int>& vector)
 	std::cout << std::endl;
 }
 
-void	print_vector_pairs(std::vector<int>& vector, size_t pairs)
+void	print_vector_pairs(std::vector<int>& vector, size_t pair_size)
 {
 	size_t	loop = 0;
 
 	std::cout << YELLOW << "(" << RT << std::flush;
 	for (std::vector<int>::iterator it = vector.begin(); it != vector.end(); it++)
 	{
-		if (loop != 0 && loop % pairs == 0)
+		if (loop != 0 && loop % pair_size == 0)
 			std::cout << YELLOW << ") (" << RT << std::flush;
 		std::cout << *it << std::flush;
-		if ((loop + 1) % pairs != 0 && (loop < vector.size() - 1))
+		if ((loop + 1) % pair_size != 0 && (loop < vector.size() - 1))
 			std::cout << ", " << std::flush;
 		loop++;
 	}
@@ -37,12 +37,12 @@ void	print_vector_pairs(std::vector<int>& vector, size_t pairs)
 }
 
 /* return the position of where to insert the insert_element */
-size_t	binary_search_ft(std::vector<int>& main, size_t start, size_t end, int insert_element, size_t pairs)
+size_t	binary_search_ft(std::vector<int>& main, size_t start, size_t end, int insert_element, size_t pair_size)
 {
 	std::cout << GREEN << "\nStart: " << RT << start << std::endl;
 	std::cout << GREEN << "end: " << RT << end << std::endl;
 	std::cout << GREEN << "insert ele: " << RT << insert_element << std::endl;
-	if (start >= end || (start + (pairs - 1) >= end))
+	if (start >= end || (start + (pair_size - 1) >= end))
 		return (start);
 
 	std::cout << YELLOW << "  Comparing (start[" << RT << start << YELLOW << "] = " \
@@ -50,20 +50,20 @@ size_t	binary_search_ft(std::vector<int>& main, size_t start, size_t end, int in
 		<< YELLOW << "] = " << RT << main[end] << YELLOW << ")" << RT << std::endl;
 
 	size_t	binary_location = 0;	
-	size_t	total_pairs_inrange = ((end - start + 1) / pairs);
-	size_t	midpoint = start + ((total_pairs_inrange / 2) * pairs) - 1;
+	size_t	total_pairs_inrange = ((end - start + 1) / pair_size);
+	size_t	midpoint = start + ((total_pairs_inrange / 2) * pair_size) - 1;
 
-//How many pairs in range?
-	// std::cout << CYAN << "Pairs value: " << RT << pairs << std::endl;
-	std::cout << CYAN << "Total pairs in this range: " << RT << total_pairs_inrange << std::endl;
+//How many pair_size in range?
+	// std::cout << CYAN << "Pairs value: " << RT << pair_size << std::endl;
+	std::cout << CYAN << "Total pair_size in this range: " << RT << total_pairs_inrange << std::endl;
 
 //calucating the midpoint values
 	std::cout << YELLOW << "midpoint[" << midpoint << "] = " << RT << main[midpoint] << std::endl;
 
 	if (insert_element < main[midpoint])
-		return binary_search_ft(main, start, midpoint, insert_element, pairs);
+		return binary_search_ft(main, start, midpoint, insert_element, pair_size);
 	else
-		return binary_search_ft(main, midpoint + 1, end, insert_element, pairs);
+		return binary_search_ft(main, midpoint + 1, end, insert_element, pair_size);
 	return (binary_location);
 };
 
@@ -72,29 +72,29 @@ int main(void)
 	std::vector<int> main = { 1, 3, 5, 7, 9, 11, 55, 100 };
 	std::vector<int> pend = { 0, 2, 4, 6, 8, 10, 29, 42 };
 	size_t	binary_location = 0;
-	size_t	pairs = 4;
+	size_t	pair_size = 4;
 
 	std::cout << "\n\nBEFORE" << std::endl;
 	std::cout << GREEN << "MAIN" << RT << std::endl;
 	print_vector(main);
 
-	for (size_t i = (pairs - 1); i < pend.size(); i += pairs)
+	for (size_t i = (pair_size - 1); i < pend.size(); i += pair_size)
 	{
 		std::cout << YELLOW << "\nstarting at pend[" << RT << i << \
 			YELLOW << "] = " << RT << pend[i] << std::endl;
-		binary_location = (binary_search_ft(main, 0, (main.size() - 1), pend[i], pairs));
+		binary_location = (binary_search_ft(main, 0, (main.size() - 1), pend[i], pair_size));
 		main.insert(main.begin() + binary_location, \
-			(pend.begin() + (i - (pairs - 1))), pend.begin() + i + 1);
-		for (size_t loop = 0; loop < pairs; loop++)
+			(pend.begin() + (i - (pair_size - 1))), pend.begin() + i + 1);
+		for (size_t loop = 0; loop < pair_size; loop++)
 			pend[i - loop] = -1;
 		std::cout << RED << "MAIN" << RT << std::endl;
-			print_vector_pairs(main, pairs);
+			print_vector_pairs(main, pair_size);
 		std::cout << "-------------------------------------------\n" << std::endl;
 	}
 	std::cout << "\n\nAFTER" << std::endl;
 	std::cout << RED << "MAIN" << RT << std::endl;
-	print_vector_pairs(main, pairs);
+	print_vector_pairs(main, pair_size);
 	std::cout << RED << "\nPend" << RT << std::endl;
-	print_vector_pairs(pend, pairs);
+	print_vector_pairs(pend, pair_size);
 	return (0);
 };
--- Homework_practice_for_cpp09/binary_search_02_jacob.cpp
+++ Homework_practice_for_cpp09/binary_search_02_jacob.cpp
@@ -12,77 +12,77 @@
 # define GREEN	"\033[32m"
 # define MAG	"\e[0;35m"
 
-void	print_vector_pairs(std::vector<int>& vector, size_t pairs)
+void	print_vector_pairs(std::vector<int>& vector, size_t pair_size)
 {
 	size_t	loop = 0;
 
 	std::cout << YELLOW << "(" << RT << std::flush;
 	for (std::vector<int>::iterator it = vector.begin(); it != vector.end(); it++)
 	{
-		if (loop != 0 && loop % pairs == 0)
+		if (loop != 0 && loop % pair_size == 0)
 			std::cout << YELLOW << ") (" << RT << std::flush;
 		std::cout << *it << std::flush;
-		if ((loop + 1) % pairs != 0 && (loop < vector.size() - 1))
+		if ((loop + 1) % pair_size != 0 && (loop < vector.size() - 1))
 			std::cout << ", " << std::flush;
 		loop++;
 	}
 	std::cout << YELLOW << ")" << RT << std::endl;
 }
 
-void	comparing_startVend(std::vector<int>& main, size_t start, size_t end, size_t pairs)
+void	comparing_startVend(std::vector<int>& main, size_t start, size_t end, size_t pair_size)
 {
-	std::cout << YELLOW << "    Comparing (start[" << RT << std::flush;
+	std::cout << YELLOW << "  Comparing (start[" << RT << std::flush;
 	
-	for (int i = 0; i != (start + pairs); i++)
+	for (int i = 0; i != (start + pair_size); i++)
 	{
 		std::cout << i << std::flush;
-		if ((i + 1) != (start + pairs))
+		if ((i + 1) != (start + pair_size))
 			std::cout << ", " << std::flush;
 	}
 	std::cout << YELLOW << "] = (" << RT << std::flush;
-	for (int i = 0; i < (start + pairs); i++)
+	for (int i = 0; i < (start + pair_size); i++)
 	{
 		std::cout << main[i] << std::flush;
-		if ((i + 1) != (start + pairs))
+		if ((i + 1) != (start + pair_size))
 			std::cout << ", " << std::flush;
 	}
 	std::cout << YELLOW << ")" << RT << std::flush;
 
 	std::cout << YELLOW << " || " << "end[" << RT << std::flush;
-	for (int i = end - 1; i != end; i++)
+	for (int i = end - 1; i < end + 1; i++)
 	{
 		std::cout << i << std::flush;
-		if ((i + 1) != end - (pairs))
+		if ((i + 1) != end + 1)
 			std::cout << ", " << std::flush;
 	}
 	std::cout << YELLOW << "] = (" << RT << std::flush;
-	for (int i = end - 1; i != end; i++)
+	for (int i = end - 1; i < end + 1; i++)
 	{
 		std::cout << main[i] << std::flush;
-		if ((i + 1) != end - pairs - 1)
+		if ((i + 1) != end + 1)
 			std::cout << ", " << std::flush;
 	}
 	std::cout << YELLOW << ")" << RT << std::endl;
 };
 
 /* return the position of where to insert the insert_element */
-size_t	binary_search_ft(std::vector<int>& main, size_t start, size_t end, int insert_element, size_t pairs)
+size_t	binary_search_ft(std::vector<int>& main, size_t start, size_t end, int insert_element, size_t pair_size)
 {
-	if (start >= end || (start + (pairs - 1) >= end))
+	if (start >= end || (start + (pair_size - 1) >= end))
 		return (start);
 
 	size_t	binary_location = 0;	
-	size_t	total_pairs_inrange = ((end - start + 1) / pairs);
-	size_t	midpoint = start + ((total_pairs_inrange / 2) * pairs) - 1;
+	size_t	total_pairs_inrange = ((end - start + 1) / pair_size);
+	size_t	midpoint = start + ((total_pairs_inrange / 2) * pair_size) - 1;
 
-	comparing_startVend(main, start, end, pairs);
+	comparing_startVend(main, start, end, pair_size);
 
-	std::cout << CYAN << "Total pairs in this range: " << RT << total_pairs_inrange << std::endl;
-	std::cout << YELLOW << "midpoint[" << midpoint << "] = " << RT << main[midpoint] << std::endl;
+	std::cout << CYAN << "    Total pair_size in this range: " << RT << total_pairs_inrange << std::endl;
+	std::cout << YELLOW << "    midpoint[" << midpoint << "] = " << RT << main[midpoint] << std::endl;
 	if (insert_element < main[midpoint])
-		return binary_search_ft(main, start, midpoint, insert_element, pairs);
+		return binary_search_ft(main, start, midpoint, insert_element, pair_size);
 	else
-		return binary_search_ft(main, midpoint + 1, end, insert_element, pairs);
+		return binary_search_ft(main, midpoint + 1, end, insert_element, pair_size);
 	return (binary_location);
 };
 
@@ -105,23 +105,55 @@ int	pend_checkall_negative(std::vector<int> pend)
 	return (1);//true
 };
 
-void	convert_pend_to_negative(std::vector<int> &pend, size_t old_jacob, size_t n, size_t pairs)
+void	convert_pend_to_negative(std::vector<int> &pend, size_t old_jacob, size_t n, size_t pair_size)
 {
 	for (int loop = n; loop > old_jacob; loop--)
 	{
-		for (int pair = 0; pair < pairs; pair++)
+		for (int pair = 0; pair < pair_size; pair++)
 		{
-			pend[(loop * pairs) - 1 - pair] = -1;
+			pend[(loop * pair_size) - 1 - pair] = -1;
 		}
 	}
 }
 
-void	print_mainpend(std::vector<int> &main, std::vector<int> &pend, size_t pairs)
+void	print_mainpend(std::vector<int> &main, \
+		std::vector<int> &pend, size_t pair_size, size_t before_after)
 {
+	if (before_after == 0)
+	{
+		std::cout << "\n------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" \
+			<< std::endl;
+		std::cout << GREEN << "---->BEFORE: " << std::endl;
+	}
+	else if (before_after == 1)
+	{
+		std::cout << GREEN << "\n---->AFTER: " << std::endl;
+	}
 	std::cout << RED << "MAIN" << RT << std::endl;
-	print_vector_pairs(main, pairs);
+	print_vector_pairs(main, pair_size);
 	std::cout << RED << "Pend" << RT << std::endl;
-	print_vector_pairs(pend, pairs);
+	print_vector_pairs(pend, pair_size);
+};
+
+
+void	insert_firstpair(std::vector<int> &main, std::vector<int> &pend, \
+			size_t &pair_size, size_t &binary_location, size_t &endpoint)
+{
+	print_mainpend(main, pend, pair_size, 0);
+	main.insert(main.begin(), (pend.begin()), pend.begin() + pair_size);
+	convert_pend_to_negative(pend, 0, 1, pair_size);
+};
+
+void	update_new_jacob(std::vector<int> &main, std::vector<int> &pend, \
+		size_t &old_jacob, size_t &old_jacobsthal, size_t &n, size_t &jacobsthal, size_t &pair_size)
+{
+	old_jacob = n;
+	old_jacobsthal = jacobsthal;
+	jacobsthal = jacob_number(++n);
+	print_mainpend(main, pend, pair_size, 1);
+	std::cout << YELLOW << "-------------------------------------------" << std::endl;
+	std::cout << "n = " << n << " | Jacobsthal = " << RT << jacobsthal << std::endl;
+	std::cout << YELLOW <<  "-------------------------------------------" << RT << std::endl;
 };
 
 int main(void)
@@ -130,7 +162,7 @@ int main(void)
 	std::vector<int> main = { 1, 3, 5, 7, 9, 11, 55, 100 };
 	std::vector<int> pend = { 0, 2, 4, 6, 8, 10, 29, 42 };
 
-	size_t	pairs = 2;
+	size_t	pair_size = 2;
 	size_t	binary_location = 0;
 
 	size_t	old_jacob = 1;
@@ -140,47 +172,37 @@ int main(void)
 
 	//inserted first pair and check if (pend.size() > jacob = 3)
 	size_t	num_of_pairs_inserted = 1;
-	size_t	endpoint = ((3 + num_of_pairs_inserted) * pairs) - 1;
-	size_t	startpoint = (pairs * 2) - 1;\
-	size_t	counter = 1;
+	size_t	endpoint = ((3 + num_of_pairs_inserted) * pair_size) - 1;
+	size_t	counter = 0;
 // ---------------------------elements--------------------------------------
 
-	print_mainpend(main, pend, pairs);
-	if (startpoint < (pend.size() - 1) && ((pairs * 3) - 1) < pend.size() - 1)
-		startpoint = (pairs * 3) - 1;
-	binary_location = (binary_search_ft(main, 0, endpoint, pend[(pairs - 1)], pairs));
-	main.insert(main.begin() + binary_location, \
-		(pend.begin() + ((pairs - 1) - (pairs - 1))), pend.begin() + (pairs - 1) + 1);
-	convert_pend_to_negative(pend, 0, 1, pairs);
-	print_mainpend(main, pend, pairs);
-	while (!pend_checkall_negative(pend))
+	insert_firstpair(main, pend, pair_size, binary_location, endpoint);
+	//while (!pend_checkall_negative(pend))
+	//cause the current issue is you have two (-1, -1) because firstpair was not considered
+	// and one extra pair was inserted cause the for loop only happened 2 times.
+	// the for loop will make it loop 4 times, and you have extra 2.
+	for (size_t i = pend.size(); i > 0 && i <= pend.size(); i -= pair_size)//create a while loop to --
 	{
-		std::cout << "------<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n" << std::endl;
-		for (size_t j = (jacobsthal * pairs) - 1; 
-			((j > (old_jacobsthal * pairs) - 1)); j -= pairs)
+		print_mainpend(main, pend, pair_size, 0);
+		for (size_t j = (jacobsthal * pair_size) - 1; 
+			((j > (old_jacobsthal * pair_size) - 1)); j -= pair_size)
 		{
 			std::cout << YELLOW << "\nstarting at pend[" << RT << j << \
 				YELLOW << "] = " << RT << pend[j] << std::endl;
 			if (j > (pend.size() - 1))
 				j = (pend.size() - 1);
-			endpoint = ((n + num_of_pairs_inserted) * pairs) - 1;
-			binary_location = (binary_search_ft(main, 0, endpoint, pend[j], pairs));
+			endpoint = ((n + num_of_pairs_inserted) * pair_size) - 1;
+			binary_location = (binary_search_ft(main, 0, endpoint, pend[j], pair_size));
 			main.insert(main.begin() + binary_location, \
-				(pend.begin() + j - (pairs - 1)), pend.begin() + j + 1);//+1 cause end iterator
+				(pend.begin() + j - (pair_size - 1)), pend.begin() + j + 1);//+1 cause end iterator
 			counter++;
-			print_mainpend(main, pend, pairs);
 		}
 		num_of_pairs_inserted += counter;
 		counter = 0;
-		convert_pend_to_negative(pend, old_jacob, n, pairs);
-		old_jacob = n;
-		old_jacobsthal = jacobsthal;
-		jacobsthal = jacob_number(++n);
-		std::cout << YELLOW << "-------------------------------------------" << std::endl;
-		std::cout << "n = " << n << " | Jacobsthal = " << RT << jacobsthal << std::endl;
-		std::cout << YELLOW <<  "-------------------------------------------" << RT << std::endl;
+		convert_pend_to_negative(pend, old_jacob, n, pair_size);
+		update_new_jacob(main, pend, old_jacob, old_jacobsthal, n, jacobsthal, pair_size);
 	}
-	print_mainpend(main, pend, pairs);
+	std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n" << std::endl;
 	return (0);
 }
 
